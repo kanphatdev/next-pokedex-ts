ffa14fb4cb50ecb1c1997fd37657dbb9c4d68728
kanphatratrazer <kanphatratrazer28@gmail.com>
2025-03-15 11:22:24 +0700

    Enable theme

--- app/grid/page.tsx
+++ app/grid/page.tsx
@@ -1,6 +1,8 @@
 export default function GridPage() {
     return (
-      <div>
+      <div className="bg-inherit
+      
+      ">
         <h1>Grid Page</h1>
         <p>This is the grid view.</p>
       </div>
